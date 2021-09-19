d85a2bea6f3139c1938667560fce2f4d2638f887
Aurelield <82478538+Aurelield@users.noreply.github.com>
2021-09-19 22:15:53 +0200

    Add files via upload

--- module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
+++ module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
@@ -6,33 +6,26 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+import random
+import sys
+import string
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+def RandomStringGenerator(l):
+    return ''.join([random.choice(string.ascii_lowercase + string.digits) for i in range(l)])
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+def BatchStringGenerator(n, m=8, M=12):
+    # n = argument positionnel = argument obligatoirement indiqué lors de l'appel de la fonction
+    # et dont l'ordre (ou la position) est fixe
+    # m, M = arguments optionnels (keyword arguments) = arguments facultativement indiqués lors de l'appel
+    # de la fonction, et s'ils ne sont pas précisés, ils prennent alors une valeur par défaut qui est celle
+    # indiquée après le "=". De plus leur ordre / position n'est pas important
+    if m > M:
+        raise ValueError('Incorrect min and max string lengths. Try again.')
+    return [RandomStringGenerator(m) if m == M 
+            else RandomStringGenerator(random.choice(range(m, M))) for i in range(n)]
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
+m = input('Enter minimum string length: ')
+M = input('Enter maximum string length: ')
 n = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(BatchStringGenerator(int(n), int(m), int(M)))
